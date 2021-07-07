754d408ef9b3bc115a427030c1e6a0942bd36112
Lenkura <62018399+Lenkura@users.noreply.github.com>
2021-07-07 19:37:09 +1000

    Added Transaction Model

--- Assignment 2/Models/Customer.cs
+++ Assignment 2/Models/Customer.cs
@@ -24,19 +24,26 @@ namespace Assignment_2.Models
         [DatabaseGenerated(DatabaseGeneratedOption.None)]
         [Range(1000, 9999)]
         public int CustomerID { get; set; }
+
         [Required, StringLength(50)]
         public string Name { get; set; }
+
         [StringLength(11)]
         public string TFN { get; set; }
+
         [StringLength(50)]
         public string Address { get; set; }
+
         [StringLength(40)]
         public string Suburb { get; set; }
+
         [StringLength(3)]
         public AusStates State { get; set; }
+
         [StringLength(4)]
         [RegularExpression(@"/\d{4}/", ErrorMessage = "Postcodes are 4 digit numbers")]
         public string PostCode { get; set; }
+
         [StringLength(12)]
         [RegularExpression(@"/^(?:\+?61|0)[2-478](?:[ -]?[0-9]){8}$/", ErrorMessage = "Please enter an Australian Phone Number")]
         public string Mobile { get; set; }
---
+++ Assignment 2/Models/Transaction.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment_2.Models
+{
+    public enum TransactionType
+    {
+        D = 1,
+        W = 2,
+        T = 3,
+        S = 4,
+        B = 5
+    }
+    public class Transaction
+    {
+        [Required]
+        public int TransactionID { get; set; }
+        [Required]
+        public TransactionType TransactionType { get; set; }
+        [Required]
+        [Column(TypeName = "char")]
+        public int AccountNumber { get; set; }
+
+        public virtual Account Account { get; set; }
+        public int? DestinationAccountNumber { get; set; }
+        public virtual Account DestinationAccount { get; set; }
+
+        [Required]
+        [Range(0.01, double.MaxValue)]
+        [Column(TypeName = "money")]
+        public decimal Amount { get; set; }
+
+        [StringLength(30)]
+        public string Comment { get; set; }
+        [Required]
+        [Column(TypeName = "datetime2")]
+        public DateTime TransactionTimeUtc { get; set; }
+    }
+}
