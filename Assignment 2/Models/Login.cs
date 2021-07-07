d11093bb569ea377be5aa128e518d4702b1a9721
Lenkura <62018399+Lenkura@users.noreply.github.com>
2021-07-07 18:40:26 +1000

    Added Login Model

---
+++ Assignment 2/Models/Login.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment_2.Models
+{
+    public class Login
+    {
+        [Required, RegularExpression(@"/\d{8}/", ErrorMessage = "LoginIds are 8 digit numbers")]
+        public string LoginID { get; set; }
+        [Required, Range(1000, 9999)]
+        public int CustomerID { get; set; }
+        public virtual Customer Customer { get; set; }
+        [Required, StringLength(64)]
+        public string PasswordHash { get; set; }
+
+    }
+}
