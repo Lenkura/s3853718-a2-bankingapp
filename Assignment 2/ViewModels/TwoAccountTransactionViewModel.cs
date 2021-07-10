48e6e9cdd2b071e8fd2b8deff7adf246b37abce9
Lenkura <62018399+Lenkura@users.noreply.github.com>
2021-07-10 20:23:17 +1000

    Added View Model for transfers

---
+++ Assignment 2/ViewModels/TwoAccountTransactionViewModel.cs
@@ -0,0 +1,23 @@
+﻿using Assignment_2.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment_2.ViewModels
+{
+    public class TwoAccountTransactionViewModel
+    {
+        public int AccountNumber { get; set; }
+        public Account Account { get; set; }
+        [Required]
+        [Range(1000, 9999, ErrorMessage = "Account not Found")]
+        public int DestinationAccountNumber { get; set; }
+        [Required]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Enter a positive amount")]
+        public decimal Amount { get; set; }
+        [StringLength(30)]
+        public string Comment { get; set; }
+    }
+}
