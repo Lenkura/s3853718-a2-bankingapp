1ed209827bb7939125b31a88cdb62f521a15a4db
Lenkura <62018399+Lenkura@users.noreply.github.com>
2021-07-17 20:14:45 +1000

    Added Readme justification for record
    - Also changed Customer to record

--- Assignment 2/Models/Customer.cs
+++ Assignment 2/Models/Customer.cs
@@ -19,36 +19,36 @@ namespace Assignment_2.Models
         SA =6,
         WA = 7
     }
-    public class Customer
+    public record Customer
     {
         [DatabaseGenerated(DatabaseGeneratedOption.None)]
         [Range(1000, 9999)]
-        public int CustomerID { get; set; }
+        public int CustomerID { get; init; }
 
         [Required, StringLength(50)]
-        public string Name { get; set; }
+        public string Name { get; init; }
 
         [StringLength(11)]
-        public string TFN { get; set; }
+        public string TFN { get; init; }
 
         [StringLength(50)]
-        public string Address { get; set; }
+        public string Address { get; init; }
 
         [StringLength(40)]
-        public string Suburb { get; set; }
+        public string Suburb { get; init; }
 
         [Column(TypeName = "nvarchar")]
-        public AusStates State { get; set; }
+        public AusStates State { get; init; }
 
         [StringLength(4)]
         [Range(1000, 9999, ErrorMessage = "Enter a Valid Postcode")]
-        public string PostCode { get; set; }
+        public string PostCode { get; init; }
 
         [StringLength(12)]
         [RegularExpression(@"04[0-9]{2}\s?[0-9]{3}\s?[0-9]{3}", ErrorMessage = "Please enter an Australian Phone Number")]
-        public string Mobile { get; set; }
+        public string Mobile { get; init; }
 
-        public virtual List<Account> Accounts { get; set; }
-        public virtual Login Login { get; set; }
+        public virtual List<Account> Accounts { get; init; }
+        public virtual Login Login { get; init; }
     }
 }
--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 Raymond Louey
 s3853718 
 Git Repository: https://github.com/rmit-wdt-sp2-2021/s3853718-a2
+
+Use of C# record type
+Found in: Transaction Model
+The record type is used to signal that an object is read-only. I've used in it the in the transaction model as I don't expect
+to change these at all as each instance of 'transaction' is a historical account of something that has happened. In terms of the
+program, knowing that records can not be changed allows for performance improvements when using them. Functions such as the statements
+can involve a large amount of transactions, which is where improvements are most visible.
