0e72e22877f1ea2d4b100e17d70d6f672d392728
Lenkura <62018399+Lenkura@users.noreply.github.com>
2021-07-19 17:30:53 +1000

    Display a list of billpays for a specific account

--- Assignment 2/Controllers/BillPayController.cs
+++ Assignment 2/Controllers/BillPayController.cs
@@ -1,6 +1,14 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Assignment_2.Authorise;
+using Assignment_2.Data;
+using Assignment_2.Models;
+using Assignment_2.ViewModels;
+using DataValidator;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,9 +16,20 @@ namespace Assignment_2.Controllers
 {
     public class BillPayController : Controller
     {
-        public IActionResult List()
+        private readonly ILogger<BillPayController> _logger;
+        private readonly Assignment2DbContext _context;
+        public BillPayController(ILogger<BillPayController> logger, Assignment2DbContext context)
         {
-            return View();
+            _logger = logger;
+            _context = context;
+        }
+
+        public async Task<IActionResult> ListAsync()
+        {
+            var customer = await _context.Customers.FindAsync(HttpContext.Session.GetInt32(nameof(Customer.CustomerID)).Value);
+            //var billpay = await _context.BillPays.FromSqlRaw(@"select b.* from BillPay b where AccountNumber").ToListAsync();
+            var billpay = await _context.BillPays.Where(x => x.AccountNumber == customer.Accounts[0].AccountNumber).ToListAsync();
+            return View(billpay);
         }
     }
 }
