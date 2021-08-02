ad3ab39b4e70fdbe1b3d3667b568bd4918cfe3fb
Lenkura <62018399+Lenkura@users.noreply.github.com>
2021-08-02 22:09:41 +1000

    Add login for admin website

--- AdminPortal/Startup.cs
+++ AdminPortal/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 
 namespace AdminPortal
@@ -23,8 +24,22 @@ namespace AdminPortal
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddHttpClient("api", client =>
+            {
+                client.BaseAddress = new Uri("https://localhost:44398");
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            });
+
+            services.AddSession(options =>
+            {
+                // Make the session cookie essential.
+                options.Cookie.IsEssential = true;
+                options.IdleTimeout = TimeSpan.FromDays(2);
+            });
+
             services.AddControllersWithViews();
         }
+
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -46,11 +61,13 @@ namespace AdminPortal
 
             app.UseAuthorization();
 
+            app.UseSession();
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Login}/{action=Index}/{id?}");
             });
         }
     }
