d949ac560acf2501f16663100d10cf8d6994ac17
Lenkura <62018399+Lenkura@users.noreply.github.com>
2021-08-04 00:49:58 +1000

    Add ability to filter account transaction by date

--- AdminPortal/Controllers/TransactionController.cs
+++ AdminPortal/Controllers/TransactionController.cs
@@ -33,29 +33,73 @@ namespace AdminPortal.Controllers
 
         public IActionResult IndexingOneAccount(int accountNumber)
         {
+            HttpContext.Session.Remove("FilterStart");
+            HttpContext.Session.Remove("FilterEnd");
+
             HttpContext.Session.SetInt32("AccountTransactions", accountNumber);
-            return RedirectToAction(nameof(OneAccount));
+            return RedirectToAction(nameof(OneAccount),
+                new DateFilterViewModel
+                {
+                    Start = DateTime.MinValue,
+                    End = DateTime.Now
+                }
+                );
         }
 
         public async Task<IActionResult> OneAccount(int? page = 1)
         {
             var accountNumber = HttpContext.Session.GetInt32("AccountTransactions");
+            ViewBag.Account = accountNumber;
 
-            var response = await Client.GetAsync($"api/Account/{accountNumber}");
+            var response = await Client.GetAsync($"api/Transaction/{accountNumber}");
             if (!response.IsSuccessStatusCode)
                 throw new Exception();
             var result = await response.Content.ReadAsStringAsync();
-            var account = JsonConvert.DeserializeObject<AccountDTO>(result);
-            ViewBag.Account = account;
-
-            response = await Client.GetAsync($"api/Transaction/{accountNumber}");
-            if (!response.IsSuccessStatusCode)
-                throw new Exception();
-            result = await response.Content.ReadAsStringAsync();
             var transactions = JsonConvert.DeserializeObject<List<TransactionDTO>>(result);
+            var filteredList = new List<TransactionDTO>();
+
+            var start = DateTime.MinValue;
+            var end = DateTime.Now;
+
+            if (HttpContext.Session.GetString("FilterStart") != null)
+                start = DateTime.Parse(HttpContext.Session.GetString("FilterStart"));
+            if (HttpContext.Session.GetString("FilterEnd") != null)
+                end = DateTime.Parse(HttpContext.Session.GetString("FilterEnd"));
+
+            foreach (var t in transactions)
+                if (start.CompareTo(t.TransactionTimeUtc) < 0 && end.CompareTo(t.TransactionTimeUtc) > 0)
+                    filteredList.Add(t);
+
             const int pageSize = 4;
-            var pagedList = transactions.ToPagedList((int)page, pageSize);
+            var pagedList = filteredList.ToPagedList((int)page, pageSize);
             return View(pagedList);
+        }
+
+        public IActionResult DateFilter()
+        {
+            var accountNumber = HttpContext.Session.GetInt32("AccountTransactions");
+            ViewBag.Account = accountNumber;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult DateFilter(DateFilterViewModel filter)
+        {
+            if (filter.Start == null)
+                filter.Start = DateTime.MinValue;
+            if (filter.End == null)
+                filter.End = DateTime.Now;
+
+            if (!(filter.Start.Value.CompareTo(DateTime.Now) < 0))
+                ModelState.AddModelError(nameof(filter.End), "Start date must be before current date");
+
+            if (!(filter.Start.Value.CompareTo(filter.End) < 0))
+                ModelState.AddModelError(nameof(filter.End), "End date should be after start date");
+            if (!ModelState.IsValid)
+                return View(filter);
+            HttpContext.Session.SetString("FilterStart", filter.Start.ToString());
+            HttpContext.Session.SetString("FilterEnd", filter.End.ToString());
+            return RedirectToAction(nameof(OneAccount));
         }
     }
 }
---
+++ AdminPortal/Models/ViewModel/DateFilterViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AdminPortal.Models
+{
+    public class DateFilterViewModel
+    {
+        public DateTime? Start { get; set; }
+
+        public DateTime? End { get; set; }
+    }
+}
