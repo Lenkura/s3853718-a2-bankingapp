9f9de875c9a7eeecab92b9e2e75c5750209ac89b
Lenkura <62018399+Lenkura@users.noreply.github.com>
2021-08-03 23:14:19 +1000

    Added Transaction view listing all transactions

--- AdminPortal/Controllers/TransactionController.cs
+++ AdminPortal/Controllers/TransactionController.cs
@@ -1,16 +1,61 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AdminPortal.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
+using X.PagedList;
 
 namespace AdminPortal.Controllers
 {
     public class TransactionController : Controller
     {
-        public IActionResult Index()
+        private readonly IHttpClientFactory _clientFactory;
+        private HttpClient Client => _clientFactory.CreateClient("api");
+        public TransactionController(IHttpClientFactory clientFactory) => _clientFactory = clientFactory;
+
+        public async Task<IActionResult> Index(int? page = 1)
         {
-            return View();
+            var response = await Client.GetAsync($"api/Transaction");
+            if (!response.IsSuccessStatusCode)
+                throw new Exception();
+            var result = await response.Content.ReadAsStringAsync();
+            var transactions = JsonConvert.DeserializeObject<List<TransactionDTO>>(result);
+            const int pageSize = 4;
+            var pagedList = transactions.ToPagedList((int)page, pageSize);
+            return View(pagedList);
+        }
+
+        public IActionResult IndexingOneAccount(int accountNumber)
+        {
+            HttpContext.Session.SetInt32("AccountTransactions", accountNumber);
+            return RedirectToAction(nameof(OneAccount));
+        }
+
+        public async Task<IActionResult> OneAccount(int? page = 1)
+        {
+            var accountNumber = HttpContext.Session.GetInt32("AccountTransactions");
+
+            var response = await Client.GetAsync($"api/Account/{accountNumber}");
+            if (!response.IsSuccessStatusCode)
+                throw new Exception();
+            var result = await response.Content.ReadAsStringAsync();
+            var account = JsonConvert.DeserializeObject<AccountDTO>(result);
+            ViewBag.Account = account;
+
+            response = await Client.GetAsync($"api/Transaction/{accountNumber}");
+            if (!response.IsSuccessStatusCode)
+                throw new Exception();
+            result = await response.Content.ReadAsStringAsync();
+            var transactions = JsonConvert.DeserializeObject<List<TransactionDTO>>(result);
+            const int pageSize = 4;
+            var pagedList = transactions.ToPagedList((int)page, pageSize);
+            return View(pagedList);
         }
     }
 }
