24a32e3b0a7bdd009923394e6363c4da55e05bb0
Lenkura <62018399+Lenkura@users.noreply.github.com>
2021-08-04 22:46:51 +1000

    Adjust Transaction Date Filter, remove extra page add on page form

--- AdminPortal/Controllers/TransactionController.cs
+++ AdminPortal/Controllers/TransactionController.cs
@@ -35,18 +35,13 @@ namespace AdminPortal.Controllers
         {
             HttpContext.Session.Remove("FilterStart");
             HttpContext.Session.Remove("FilterEnd");
+            HttpContext.Session.Remove("FilterPageNumber");
 
             HttpContext.Session.SetInt32("AccountTransactions", accountNumber);
-            return RedirectToAction(nameof(OneAccount),
-                new DateFilterViewModel
-                {
-                    Start = DateTime.MinValue,
-                    End = DateTime.Now
-                }
-                );
+            return RedirectToAction(nameof(OneAccount));
         }
 
-        public async Task<IActionResult> OneAccount(int? page = 1)
+        public async Task<IActionResult> OneAccount(DateTime? DateFilterStart, DateTime? DateFilterEnd, int? page = 1)
         {
             var accountNumber = HttpContext.Session.GetInt32("AccountTransactions");
             ViewBag.Account = accountNumber;
@@ -61,10 +56,29 @@ namespace AdminPortal.Controllers
             var start = DateTime.MinValue;
             var end = DateTime.Now;
 
+            if (page == 1 && HttpContext.Session.GetInt32("FilterPageNumber").HasValue)
+            {
+                HttpContext.Session.Remove("FilterStart");
+                HttpContext.Session.Remove("FilterEnd");
+            }
+            HttpContext.Session.SetInt32("FilterPageNumber", page.Value);
+
             if (HttpContext.Session.GetString("FilterStart") != null)
                 start = DateTime.Parse(HttpContext.Session.GetString("FilterStart"));
             if (HttpContext.Session.GetString("FilterEnd") != null)
                 end = DateTime.Parse(HttpContext.Session.GetString("FilterEnd"));
+
+            if (DateFilterStart != null)
+            {
+                start = DateFilterStart.Value;
+                HttpContext.Session.SetString("FilterStart", DateFilterStart.Value.ToString());
+            }
+
+            if (DateFilterEnd != null)
+            {
+                end = DateFilterEnd.Value;
+                HttpContext.Session.SetString("FilterEnd", DateFilterEnd.Value.ToString());
+            }
 
             foreach (var t in transactions)
                 if (start.CompareTo(t.TransactionTimeUtc) < 0 && end.CompareTo(t.TransactionTimeUtc) > 0)
@@ -73,33 +87,6 @@ namespace AdminPortal.Controllers
             const int pageSize = 4;
             var pagedList = filteredList.ToPagedList((int)page, pageSize);
             return View(pagedList);
-        }
-
-        public IActionResult DateFilter()
-        {
-            var accountNumber = HttpContext.Session.GetInt32("AccountTransactions");
-            ViewBag.Account = accountNumber;
-            return View();
-        }
-
-        [HttpPost]
-        public IActionResult DateFilter(DateFilterViewModel filter)
-        {
-            if (filter.Start == null)
-                filter.Start = DateTime.MinValue;
-            if (filter.End == null)
-                filter.End = DateTime.Now;
-
-            if (!(filter.Start.Value.CompareTo(DateTime.Now) < 0))
-                ModelState.AddModelError(nameof(filter.End), "Start date must be before current date");
-
-            if (!(filter.Start.Value.CompareTo(filter.End) < 0))
-                ModelState.AddModelError(nameof(filter.End), "End date should be after start date");
-            if (!ModelState.IsValid)
-                return View(filter);
-            HttpContext.Session.SetString("FilterStart", filter.Start.ToString());
-            HttpContext.Session.SetString("FilterEnd", filter.End.ToString());
-            return RedirectToAction(nameof(OneAccount));
         }
     }
 }
