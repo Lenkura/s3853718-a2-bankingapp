76dac18b5a268d5d607e16e77d0f069c3286571e
Lenkura <62018399+Lenkura@users.noreply.github.com>
2021-08-09 19:37:31 +1000

    Changed mobile error message to be format specific

--- Assignment 2/Models/Customer.cs
+++ Assignment 2/Models/Customer.cs
@@ -54,7 +54,7 @@ namespace MvcMCBA.Models
         public string PostCode { get; set; }
 
         [StringLength(12)]
-        [RegularExpression(@"04[0-9]{2}\s[0-9]{3}\s[0-9]{3}", ErrorMessage = "Please enter an Australian Phone Number")]
+        [RegularExpression(@"04[0-9]{2}\s[0-9]{3}\s[0-9]{3}", ErrorMessage = "Please enter an Australian Phone Number in the format 04XX XXX XXX")]
         public string Mobile { get; set; }
         [Column(TypeName = "nvarchar")]
         public CustomerStatus Status { get; set; }
