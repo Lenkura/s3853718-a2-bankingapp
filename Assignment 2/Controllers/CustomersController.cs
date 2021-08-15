d3585f32df38ce23e5059c003e7e382760d7d060
Lenkura <62018399+Lenkura@users.noreply.github.com>
2021-08-15 16:16:13 +1000

    Change password uses asp identity. Remove old login logic

--- Assignment 2/Controllers/CustomersController.cs
+++ Assignment 2/Controllers/CustomersController.cs
@@ -17,11 +17,9 @@ namespace MvcMCBA.Controllers
     public class CustomersController : Controller
     {
         private readonly MCBAContext _context;
-        private readonly UserManager<ApplicationUser> _userManager;
-        public CustomersController(MCBAContext context, UserManager<ApplicationUser> userManager)
+        public CustomersController(MCBAContext context)
         {
             _context = context;
-            _userManager = userManager;
         }
 
 
@@ -116,56 +114,6 @@ namespace MvcMCBA.Controllers
                 return RedirectToAction(nameof(Details));
             }
             return View(customer);
-        }
-
-        public IActionResult Password()
-        {
-            return View();
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> Password(ChangePasswordViewModel viewModel)
-        {
-            var loginID = HttpContext.Session.GetString("LoginID");
-            var login = await _context.Logins.FindAsync(loginID);
-            if (login == null || !PBKDF2.Verify(login.PasswordHash, viewModel.OldPasswordHash))
-            {
-                ModelState.AddModelError(nameof(viewModel.OldPasswordHash), "Incorrect Password");
-                return View();
-            }
-
-            if (viewModel.NewPasswordHash1 != viewModel.NewPasswordHash2)
-            {
-                ModelState.AddModelError("PasswordChange", "New Passwords did not Match");
-                return View();
-            }
-            if (!ModelState.IsValid)
-                return View();
-            login.PasswordHash = PBKDF2.Hash(viewModel.NewPasswordHash1);
-            try
-            {
-                _context.Update(login);
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (_context.Logins.Any(x => x.LoginID == login.LoginID))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            var user = _userManager.FindByNameAsync(HttpContext.Session.GetString("LoginID")).Result;
-            var password = new PasswordHasher<ApplicationUser>();
-            var passHash = password.HashPassword(user,viewModel.NewPasswordHash1);
-            user.PasswordHash = passHash;
-            var result = _userManager.UpdateAsync(user);
-
-            return RedirectToAction("Logout", "Login");
         }
 
         // GET: Customers/Delete/5
